c31cba86b1f4370194f84e4299f3ecae60bb0c8e
unknown <luca.francesconi1977@outlook.com>
2020-04-19 00:13:26 +0200

    Aggiunti blocchi try/except per gestione eccezioni - Aggiunte voci menu 3/4 per estrazione pagine pari/dispri da PDF (ancora da implemnetare il codice che eseguo queste due estrazioni) - Tradotti messaggi da italiano ad inglese.

--- PyExtractFromPdf.py
+++ PyExtractFromPdf.py
@@ -14,91 +14,147 @@ def main():
         outPdf = ''
         
         #Parametri di input
-        inpPDF = input('Indicare pdf di origine: ')
-        inpPages = input('Indicare numero pagine/e da estrarre (es: 1/1,2,3/1-3): ')
+        inpPDF = input('Source PDF: ')
         
-        #Nel caso da input ricevo numero pagine tipo 1-3 allora cerco carattere '-' oppure ',' e lo sostituisco con ' '
-        if inpPages.find('-') | inpPages.find(','):
-            inpPages = inpPages.replace('-',' ').replace(',',' ') 
-            #Inserisco in una lista i valori letti da input e normalizzati
-            pageCounter = inpPages.split(' ')
+        try:
+            #Apro file in binary/lettura (PDF Origine)
+            f = open(inpPDF, "br")
+      
         
-        #Creo un tange con elementi inseriti nella lista precedentemente e ciclo per aggiungere a
-        #lista pages le pagine che devo estrarre (in python il primo indice )
-        for page in range(int(pageCounter[0]),int(pageCounter[-1]) + 1):
-            pages.insert(page-1,page-1)
+            inpPages = input('Page/pages to extract (ex: 1/1,2,3/1-3): ')
+            
+            #Nel caso da input ricevo numero pagine tipo 1-3 allora cerco carattere '-' oppure ',' e lo sostituisco con ' '
+            if inpPages.find('-') | inpPages.find(','):
+                inpPages = inpPages.replace('-',' ').replace(',',' ') 
+                #Inserisco in una lista i valori letti da input e normalizzati
+                pageCounter = inpPages.split(' ')
+            
+            #Creo un tange con elementi inseriti nella lista precedentemente e ciclo per aggiungere a
+            #lista pages le pagine che devo estrarre (in python il primo indice )
+            for page in range(int(pageCounter[0]),int(pageCounter[-1]) + 1):
+                pages.insert(page-1,page-1)
 
-        f = open(inpPDF, "br")
-        
-        while outPdf == '':
-            outPdf = input('Indicare pdf di destinazione: ').replace('\n','')
-        
-        fOut = open(outPdf, "bw")
-        extractPage(f, fOut, pages)
+            
+            while outPdf == '':
+                outPdf = input('Destination PDF: ').replace('\n','')
+            
+            #Apro file in binary/scrittura (PDF destinazione)
+            fOut = open(outPdf, "bw")
+
+            res = extractPage(f, fOut, pages)
+            
+            if res:
+                print ('File created correctly.')
+                input()
+                main()
+                
+        except IOError:
+            print("File not accessible!")
+            input()
+            main()
         
     elif s  == '2':
         
         pages = []
         outPdf = ''
-        inpPDF = input('Indicare pdf di origine: ')
-        xString = input('Indicare testo da ricercare: ')
-        f = open(inpPDF, "rb")
-        #Chiamo funzione per cercare all'interno del PDF indicato da input
-        pages = findInPdf(f, xString)
+        inpPDF = input('Source PDF: ')
+        xString = input('Text to search: ')
         
-        if  len(pages) != 0:
-            
-            while outPdf == '':
-                outPdf = input('Indicare pdf di destinazione: ').replace('\n','')
-                 
-            fOut = open(outPdf, "bw")
-            #Chiamo funzione per estrarre le pagine dove ho trovato il testo
-            extractPage(f,fOut,pages)
-        else:
-            print ('Non è stata trovata alcuna pagina contenete il testo richiesto')
+        try:
+            #Apro file in binary/lettura (PDF Origine)
+            f = open(inpPDF, "rb")
+            #Chiamo funzione per cercare all'interno del PDF indicato da input
+            pages = findInPdf(f, xString)
+        
+            if  len(pages) != 0:
+                
+                while outPdf == '':
+                    outPdf = input('Indicare pdf di destinazione: ').replace('\n','')
+                
+                fOut = open(outPdf, "bw")
+                #Chiamo funzione per estrarre le pagine dove ho trovato il testo
+                res = extractPage(f,fOut,pages)
+                
+                if res:
+                    print ('File created correctly.')
+                    input()
+                    main()
+            else:
+                print ('No page was found containing the requested text.')
+                input()
+                drawMenu()
+                
+        except IOError:
+            print("File not accessible!")
             input()
-            drawMenu()  
-        
+            main()
+        except Exception:
+            print("An error has occured!")
+            input()
+            main()
+    elif s  == '3':
+        main()
+    elif s  == '4':
+        main()
     else:
+        print("Bye!:-)")
         exit()
-    
 
 # extract pages
 def extractPage(f, fOut, pages):
 
-    pdfOutput = PdfFileWriter()
-    pdfInput = PdfFileReader(f)
+    try:
+        
+        pdfOutput = PdfFileWriter()
+        pdfInput = PdfFileReader(f)
 
-    for i in pages:
-        pdfOutput.addPage(pdfInput.getPage(i))
+        for i in pages:
+            pdfOutput.addPage(pdfInput.getPage(i))
 
-    pdfOutput.write(fOut)
-    fOut.close()
+        pdfOutput.write(fOut)
+        fOut.close()
+        f.close()
+        return True
+    
+    except Exception:
+        print("An error has occured!")
+        input()
+        main()
 
 def findInPdf(f, xString):
     
     pages = []
     pdfDoc = PdfFileReader(f)
     
-    for i in range(0, pdfDoc.getNumPages()):
+    try:
         
-        content = ""
-        content += pdfDoc.getPage(i).extractText()
-        
-        # content1 = content.encode('ascii', 'ignore').lower()
-        # ResSearch = re.search(xString, content1)
-        
-        if re.search(xString,content):
-           pages.append(i)
-           print ('Testo trovato a pagine: ' + str(i))
-           
-    return pages
+        for i in range(0, pdfDoc.getNumPages()):
+            
+            content = ""
+            content += pdfDoc.getPage(i).extractText()
+            
+            # content1 = content.encode('ascii', 'ignore').lower()
+            # ResSearch = re.search(xString, content1)
+            
+            if re.search(xString,content):
+                pages.append(i)
+                print ('Testo trovato a pagine: ' + str(i))
+            
+        return pages
+    
+    except Exception:
+        print("An error has occured!")
+        input()
+        main()
 
 def drawMenu():
     print('\n ::—–{ Menu }—-::')
-    print('\n1 - Estrai pagine da PDF')
-    print('2 - Cerca testo in PDF ed estrai pagine')
-    print('3 - Termina il programma.')
+    print('\n1 - Extract page / pages from PDF')
+    print('2 - Extract page / pages from PDF contain specific text')
+    print('3 - Extract even pages from PDF')
+    print('4 - Extract even pages from PDF')
+    print('5 - Search for text and extract pages')
+    print('0 - Quit')
     print('\n ::—–{ :-)) }—-::')
     s = (input('\n Seleziona una voce del menu: '))
     return s
